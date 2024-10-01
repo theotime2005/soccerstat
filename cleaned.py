623d3414e626f174330801eb58e3ef66c3915e2c
theotime2005 <theotime.berthod@epitech.digital>
2024-10-01 12:06:59 +0200

    Ajout d'une fonction pour supprimer les valeurs abérentes
    Co-Authored-By: marquitoo10 <181709257+marquitoo10@users.noreply.github.com>

--- cleaned.py
+++ cleaned.py
@@ -6,7 +6,22 @@ data = pd.read_csv(f"{file_path}.csv")
 data_cleaned = data.drop_duplicates()
 
 data_cleaned = data_cleaned.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+data_cleaned.drop(['Rk'], axis=1, inplace=True)
 
 data_cleaned = data_cleaned.dropna()
+
+def remove_outliers(df, column_name):
+    # Calculer l'écart-type de la colonne
+    std_dev = df[column_name].std()
+    
+    # Filtrer les lignes où la valeur est entre -écart-type et +écart-type
+    filtered_df = df[(df[column_name] >= -std_dev) & (df[column_name] <= std_dev)]
+    
+    return filtered_df
+
+
+check_column=["G-PK", "G+A", "Ast"]
+for column in check_column:
+    data_cleaned = remove_outliers(data_cleaned, column)
 
 data_cleaned.to_csv(f"{file_path}-cleaned.csv", index=False)
